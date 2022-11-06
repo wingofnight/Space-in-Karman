489e5eb967dac862f62fa2eafd312a60dd3cab6d
Владимир <51334865+wingofnight@users.noreply.github.com>
2022-11-06 10:20:53 +0300

    StartScene
    Armors change position

---
+++ Assets/Scripts/ArmorUp1.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmorUp1 : MonoBehaviour
+{
+   public Toggle tg;
+   
+   public GameObject armor;
+   public float startPosition,endPosition;
+   
+   private Vector3 pos = new Vector3();
+    
+   
+     //  startPosition += 653;
+      // endPosition += 653;
+
+    private void FixedUpdate() {
+       if(!tg.isOn && armor.transform.position.y < endPosition)
+       armor.transform.Translate(Vector3.up * Time.deltaTime*50,Space.World); 
+       else if(armor.transform.position.y > startPosition){
+       armor.transform.Translate(Vector3.down * Time.deltaTime*50,Space.World); 
+       }
+        print(armor.transform.position.y + "   !    ");
+    }
+     
+}
