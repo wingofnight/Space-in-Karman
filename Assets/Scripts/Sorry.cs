f21b8465ea7b8e2070ad09d4d6ecb32bcb9edfe1
Владимир <51334865+wingofnight@users.noreply.github.com>
2022-11-06 22:10:56 +0300

    borsh

---
+++ Assets/Scripts/Sorry.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sorry : MonoBehaviour
+{
+    public Text txt;
+   public void changeText(){
+    txt.text = "Приносим извинения! Полная версия игра будет в патчах на этой странице";
+    }
+}
