3157174637f92c0c49dc84090737d165d1c35005
Владимир <51334865+wingofnight@users.noreply.github.com>
2023-02-01 04:18:53 +0300

    8

---
+++ Assets/Scripts/ChengeBackground.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChengeBackground : MonoBehaviour
+{
+    [Header ("Set in Inspector")]
+
+   public GameObject button = new GameObject();
+    
+   public Sprite onButton;
+        
+    public Sprite ofButton;
+   
+}
